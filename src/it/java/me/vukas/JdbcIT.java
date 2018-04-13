20b77dbdd043b7889e833be24b8ab080e0b7ef67
Bojan Vukasovic <b.vukasovic@sportradar.com>
2018-04-13 14:49:50 +0200

    refactored to it tests

--- src/it/java/me/vukas/JdbcIT.java
+++ src/it/java/me/vukas/JdbcIT.java
@@ -7,9 +7,9 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.jdbc.Sql;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringJUnit4ClassRunner.class)
+@RunWith(SpringRunner.class)
 @ContextConfiguration(classes = { ProgramConfig.class, TestProgramConfig.class })
 public class JdbcIT {
 
---
+++ src/test/java/me/vukas/UnitTests.java
@@ -0,0 +1,12 @@
+package me.vukas;
+
+import org.junit.Test;
+
+public class UnitTests {
+
+	@Test
+	public void ut(){
+
+	}
+
+}
